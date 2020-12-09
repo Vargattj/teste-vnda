8ec7a1b87d0c865ac37561fb2f4cedac9f91c65a
vargattj <vargattjh@gmail.com>
2020-12-09 19:04:52 -0300

    create scroll section animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,7 @@
 import hamburguerMenu from "./modules/hamburguerMenu.js";
+import swiper from "./modules/swiper.js";
+import productSwiper from "./modules/productSwiper.js";
 
 hamburguerMenu();
+swiper();
+productSwiper();
